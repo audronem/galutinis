b4c7ff5ce784d86198a19f59140d80f667e361e4
audronem <78819371+audronem@users.noreply.github.com>
2021-03-09 23:26:13 +0200

    Create studentai.h

---
+++ studentai.h
@@ -0,0 +1,18 @@
+#ifndef STUDENTOINFO_H
+#define STUDENTOINFO_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+struct studentoinfo{
+    std::string vardas;
+    std::string pavarde;
+    int pazymiusk;
+    double vid;
+    double gal;
+    int egz;
+    std::vector<int> pazymiai;
+};
+
+#endif
