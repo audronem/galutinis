469cf63468f3fffad4369e07510682698cf2a012
audronem <78819371+audronem@users.noreply.github.com>
2021-04-25 20:21:36 +0300

    Update funkcijos.h

--- funkcijos.h
+++ funkcijos.h
@@ -48,4 +48,37 @@ void spausdinimaslist(std::list<studentoinfo>& studentai, std::string& pavadinim
 
 void spausdinimasdeque(std::deque<studentoinfo>& studentai, std::string& pavadinimas);
 
+template<typename T>
+void nuskaitymas(T& studentai, std::string failopav){
+    std::ifstream f2;
+
+    try{
+        f2.open (failopav);
+        if(f2.fail()){
+            throw "Klaida atidarant faila.";
+        }
+    }
+
+    catch (std::string fail) {
+        std::cout << fail << std::endl;
+        exit(1);
+    }
+
+    int paz;
+    std::string eil;
+        while (getline(f2, eil)){
+            studentoinfo st2;
+            std::istringstream iss(eil);
+            iss>>st2.vardas;
+            iss>>st2.pavarde;
+                while(iss>>paz){
+                    st2.pazymiai.push_back(paz);
+                }
+            st2.pazymiai.pop_back();
+            st2.egz=paz;
+            studentai.push_back(st2);
+        }
+    f2.close();
+}
+
 #endif
